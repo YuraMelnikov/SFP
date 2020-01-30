c9236f835185ab6d73ec765120ff8352d5d0032d
YuraMelnikov <yury.melnikau@gmail.com>
2020-01-30 15:39:42 +0300

    -

--- Repository/ImageRepository.cs
+++ Repository/ImageRepository.cs
@@ -1,8 +1,6 @@
 ﻿using Contracts;
 using Entities;
 using Entities.Models;
-using System;
-using System.Linq;
 
 namespace Repository
 {
---
+++ WebApiTests/Fake/ImageServiceFake.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Contracts;
+using Entities.Models;
+
+namespace WebApiTests.Fake
+{
+    public class ImageServiceFake
+    {
+        private readonly List<Image> _images;
+
+        public ImageServiceFake()
+        {
+            _images = new List<Image>()
+            {
+                new Image() { Id = new Guid("ab2bd817-98cd-4cf3-a80a-53ea0cd9c200"), Description = "desc1", Link = "/1/2/3"},
+                new Image() { Id = new Guid("815accac-fd5b-478a-a9d6-f171a2f6ae7f"), Description = "desc2", Link = "/4/5/6"},
+                new Image() { Id = new Guid("33704c4a-5b87-464c-bfb6-51971b4d18ad"), Description = "desc3", Link = "/7"}
+            };
+        }
+    }
+}
